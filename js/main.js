dcc84992fc9bac7d48757d09f9ae1d3ec7cc33d2
menhunging <menhunging@gmail.com>
2024-04-08 14:43:54 +0300

    next block

--- js/main.js
+++ js/main.js
@@ -118,37 +118,191 @@ $(document).ready(function () {
     });
   }
 
-  if ($(".slider-reviews").length > 0) {
-    const swiper = new Swiper(".slider-reviews", {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      initialSlide: $(".slider-reviews .swiper-slide").length - 1,
+  if ($(".slider-collection").length > 0) {
+    const swiper = new Swiper(".slider-collection", {
+      slidesPerView: 3,
+      initialSlide: 3,
+      spaceBetween: 24,
+      centeredSlides: true,
       watchSlidesProgress: true,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".collection-main .arrows-controls__right",
+        prevEl: ".collection-main .arrows-controls__left",
       },
       breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-          initialSlide: 0,
+        0: {
+          slidesPerView: 1.28,
+          initialSlide: 3,
+          spaceBetween: 16,
         },
         768: {
-          slidesPerView: 1,
-          spaceBetween: 10,
+          slidesPerView: 3,
+          initialSlide: 3,
+          spaceBetween: 8,
+        },
+        1024: {
+          slidesPerView: 3,
+          initialSlide: 3,
+          spaceBetween: 16,
+        },
+        1280: {
+          slidesPerView: 3,
+          initialSlide: 3,
+          spaceBetween: 24,
         },
       },
     });
+
+    setHeightBlock();
+
+    $(window).on("resize", function () {
+      setHeightBlock();
+    });
+
+    function setHeightBlock() {
+      if ($(window).width() < 768) {
+        let height =
+          $(".slider-collection .swiper-slide-active").outerHeight() + 30;
+        console.log(height);
+        $(".slider-collection").css("min-height", height);
+      } else {
+        $(".slider-collection").css("min-height", 0);
+      }
+    }
   }
 
-  if ($("[data-aos]").length) {
+  if ($(".slider-design").length > 0) {
+    const swiper = new Swiper(".slider-design", {
+      slidesPerView: "auto",
+      initialSlide: 1,
+      spaceBetween: 16,
+      centeredSlides: true,
+      watchSlidesProgress: true,
+      navigation: {
+        nextEl: ".main-design-section .arrows-controls__right",
+        prevEl: ".main-design-section .arrows-controls__left",
+      },
+      // breakpoints: {
+      //   0: {
+      //     slidesPerView: 1.28,
+      //     initialSlide: 3,
+      //     spaceBetween: 16,
+      //   },
+      //   768: {
+      //     slidesPerView: 3,
+      //     initialSlide: 3,
+      //     spaceBetween: 8,
+      //   },
+      //   1024: {
+      //     slidesPerView: 3,
+      //     initialSlide: 3,
+      //     spaceBetween: 16,
+      //   },
+      //   1280: {
+      //     slidesPerView: 3,
+      //     initialSlide: 3,
+      //     spaceBetween: 24,
+      //   },
+      // },
+    });
+  }
+
+  if ($("[data-aos]").length > 0) {
     $("[data-aos]").each((i, el) => {
       // $(el).attr("data-aos-offset", 0);
       AOS.init({
         duration: 2000,
         once: true,
       });
+    });
+  }
+
+  if ($(".catalog-section__links").length > 0) {
+    $(".catalog-section .arrows-controls__left").on("click", function () {
+      let index = $(".catalog-section__content.active").index() - 1;
+
+      handle(index);
+      mobileHandle($(this), index);
+    });
+
+    $(".catalog-section .arrows-controls__right").on("click", function () {
+      let index = $(".catalog-section__content.active").index() + 1;
+
+      if (index >= $(".catalog-section__content").length) {
+        index = 0;
+      }
+
+      handle(index);
+      mobileHandle($(this), index);
+    });
+
+    $(".catalog-section__links a").hover(function () {
+      let self = $(this);
+      let catalog = $(this).attr("data-catalog");
+
+      $(".catalog-section__links a").removeClass("active");
+      $(".catalog-section__content").removeClass("active");
+
+      self.addClass("active");
+      $(`#${catalog}`).addClass("active");
+    });
+
+    setHeightBlock();
+
+    function handle(index) {
+      $(".catalog-section__links a").removeClass("active");
+      $(".catalog-section__content").removeClass("active");
+
+      $(".catalog-section__content").eq(index).addClass("active");
+      $(".catalog-section__links li").eq(index).find("a").addClass("active");
+    }
+
+    function mobileHandle(arrow, index) {
+      if ($(window).width() < 768) {
+        let container = $(".catalog-section__links");
+        let block = $(".catalog-section__links li").eq(index).find("a");
+        let containerOuterWidth = container.outerWidth(); // узнаем ширину контейнера (width + padding)
+        let itemOuterWidth = block.outerWidth(); // узнаем ширину текущего элемента (width + padding)
+        let itemOffsetLeft = block.offset().left; // узнаем значение отступа слева в контейнере у текущего элемента
+        let containerScrollLeft = container.scrollLeft(); // узнаем текущее значение скролла
+        let speedAnimate = 400; //скорость
+        let positionCetner = containerOuterWidth / 2 - itemOuterWidth / 2 + 25; // рассчитываем позицию центра
+        console.log(positionCetner);
+        let scrollLeftUpd =
+          containerScrollLeft + itemOffsetLeft - positionCetner; // рассчитываем положение скролла относительно разницы отступа элемента и центра контейнера
+
+        arrow.addClass("disabled");
+
+        setHeightBlock();
+
+        setTimeout(function handleDisabled() {
+          arrow.removeClass("disabled");
+        }, speedAnimate);
+
+        container.animate(
+          {
+            scrollLeft: scrollLeftUpd,
+          },
+          speedAnimate
+        );
+      }
+    }
+
+    function setHeightBlock() {
+      if ($(window).width() < 768) {
+        let catalog = $(".catalog-section__content.active").find(
+          ".catalog-main-item"
+        );
+        let height =
+          $(catalog[0]).outerHeight() + $(catalog[1]).outerHeight() + 16; // +16px отступ
+        $(".catalog-section__catalog").height(height);
+      } else {
+        $(".catalog-section__catalog").attr("style", "");
+      }
+    }
+
+    $(window).on("resize", function () {
+      setHeightBlock();
     });
   }
 });
